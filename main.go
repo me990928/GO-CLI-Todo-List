241eed59c3f0abebaa66ccc600d879c68ab82e16
me990928 <me990928@gmail.com>
2023-08-03 06:35:41 +0900

    todoリスト確認画面作成&todo完了機能追加

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
+
 	"time"
 
 	"github.com/manifoldco/promptui"
+	// "golang.org/x/text/search"
 )
 
 type Todo struct {
@@ -14,59 +18,103 @@ type Todo struct {
 	Body      string `json:"body"`
 	StartDate string `json:"stDate"`
 	EndDate   string `json:"edDate"`
+	IsTodo    bool   `json:"isTodo"`
 }
 
 func main() {
-	prompt := promptui.Select{
-		Label: "Menu",
-		Items: []string{"Add Todo", "Show Todo"},
-	}
-
-	_, res, err := prompt.Run()
-
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-
-	if res == "Add Todo" {
-		fmt.Printf("Title：")
-		var title string
-		fmt.Scan(&title)
-		fmt.Printf("Body：")
-		var body string
-		fmt.Scan(&body)
-		var startDate = time.Now().Format("2006-01-02")
-		fmt.Printf("Start day： %s\n", startDate) // debug
-		var date int = 0
-		fmt.Printf("How many days?：")
-		fmt.Scan(&date)
-		var endDate = time.Now().AddDate(0, 0, date).Format("2006-01-02")
-		fmt.Printf("endDate:%s", endDate)
-
+	for {
 		prompt := promptui.Select{
-			Label: "What to save?",
-			Items: []string{"Yes", "No"},
+			Label: "Menu",
+			Items: []string{"Add Todo", "Show Todo", "Exit"},
 		}
 
 		_, res, err := prompt.Run()
 
 		if err != nil {
-			fmt.Println("err")
+			fmt.Printf("Prompt failed %v\n", err)
+			continue
 		}
 
-		if res == "Yes" {
-			todo := []Todo{
-				{Title: title, Body: body, StartDate: startDate, EndDate: endDate},
-			}
-
-	                fmt.Println("Writing Start!")
-			writeJson(todo)
-
-		} else {
-			main()
+		switch res {
+		case "Add Todo":
+			addTodo()
+		case "Show Todo":
+			showTodo()
+		case "Exit":
+			fmt.Println("Bye...")
+			return
+		default:
+			fmt.Println("Err...")
+			return
 		}
+
 	}
+}
+
+func addTodo() {
+
+	fmt.Printf("Title：")
+	var title string
+	fmt.Scan(&title)
+	fmt.Printf("Body：")
+	var body string
+	fmt.Scan(&body)
+	var startDate = time.Now().Format("2006-01-02")
+	fmt.Printf("Start day： %s\n", startDate) // debug
+	var date int = 0
+	fmt.Printf("How many days?：")
+	_, err := fmt.Scan(&date)
+	if err != nil {
+		fmt.Println("Non-numeric input confirmed.")
+		return
+	}
+	var endDate = time.Now().AddDate(0, 0, date).Format("2006-01-02")
+	fmt.Printf("endDate:%s", endDate)
+	var isTodo = false
+
+	template := &promptui.SelectTemplates{
+		Label: "{{.}}",
+		Details: `
+Title: ` + title + `
+Body: ` + body + `
+Start day: ` + startDate + `
+End Date: ` + endDate + `
+Date: ` + fmt.Sprintf("%d", date),
+	}
+
+	prompt := promptui.Select{
+		Label:     "What to save?",
+		Templates: template,
+		Items:     []string{"Yes", "No"},
+	}
+
+	_, res, err := prompt.Run()
+
+	if err != nil {
+		fmt.Println("err")
+	}
+
+	if res == "Yes" {
+		todo := []Todo{
+			{Title: title, Body: body, StartDate: startDate, EndDate: endDate, IsTodo: isTodo},
+		}
+
+		fmt.Println("Writing Start!")
+		writeJson(todo)
+		fmt.Println("Write Todo Success!")
+
+	}
+
+}
+
+func showTodo() {
+	var todos = []Todo{}
+	todos, err := readJson()
+	if err != nil {
+		fmt.Println("Err: ", err)
+		return
+	}
+	dispAll(todos)
 }
 
 func writeJson(data []Todo) {
@@ -137,6 +185,94 @@ func writeJson(data []Todo) {
 		return
 	}
 
-	fmt.Println("Write Todo Success!")
+}
 
+func readJson() ([]Todo, error) {
+
+	var todoData = []Todo{}
+
+	file, err := os.Open("todo.json")
+	if err != nil {
+		return todoData, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return todoData, err
+	}
+
+	err = json.Unmarshal(data, &todoData)
+	if err != nil {
+		return todoData, err
+	}
+
+	return todoData, nil
+}
+
+func dispAll(todos []Todo) {
+
+	const defDate = "2006-01-02"
+	// todos[0].
+	template := &promptui.SelectTemplates{
+		Label:    "{{ . }}",
+		Active:   "✔️ {{ .Title | green }}",
+		Inactive: "{{ .Title}}",
+		Selected: "✔️ {{ .Title | cyan }}",
+		Details: `
+============================
+Body: {{.Body}}
+StartDate: {{.StartDate}}
+EndDate: {{.EndDate}}
+IsTodo: {{.IsTodo}}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		todo := todos[index]
+		name := strings.Replace(strings.ToLower(todo.Body), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Todo List",
+		Items:     todos,
+		Templates: template,
+		Size:      4,
+		Searcher:  searcher,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Err: ", err)
+		return
+	}
+
+	prompt2 := promptui.Select{
+		Label:     "Complete todo?",
+		Templates: template,
+		Items:     []string{"Yes", "No"},
+	}
+
+	_, res, err1 := prompt2.Run()
+
+	if err1 != nil {
+		fmt.Printf("Err: ", err1)
+		return
+	}
+
+	if res == "Yes" {
+		todos[i].IsTodo = true
+		writeJson(todos)
+	} else {
+		todos[i].IsTodo = false
+		writeJson(todos)
+	}
+
+	fmt.Println(res)
+
+	return
 }
